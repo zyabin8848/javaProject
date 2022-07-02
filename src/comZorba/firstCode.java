d88e6e8999dddbffc5d1623ce61e6f2262a7eaa4
zyabin8848 <j.khanal65@gmail.com>
2022-07-01 20:45:28 -0500

    this is the intelij last

---
+++ src/comZorba/firstCode.java
@@ -0,0 +1,11 @@
+package comZorba;
+
+public class firstCode {
+    public static void main(String[] args) {
+        System.out.println("this is first programm");
+
+        int num1 = 45;
+        int num2 = 65;
+        System.out.println(num1+ num2);
+    }
+}
